c041c51541144568071c623ecbd946e93584791c
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-05-17 16:09:49 +0200

    finished week3 querys

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -8,19 +8,19 @@ const knex = require("../database");
 |--------------------------------------------------
 */
 
-routerMeals.get("/", async (req, res) => {
-  try {
-    const allMeals = await knex("Meal").select("*");
-    if (!allMeals.length) {
-      res.status(404).json({ error: "That shit doesn’t exist." });
-    }
-    res.json(allMeals);
-  } catch (error) {
-    res.status(500).json({
-      error: "Error while looking for the meal",
-    });
-  }
-});
+// routerMeals.get("/", async (req, res) => {
+//   try {
+//     const allMeals = await knex("Meal").select("*");
+//     if (!allMeals.length) {
+//       res.status(404).json({ error: "That shit doesn’t exist." });
+//     }
+//     res.json(allMeals);
+//   } catch (error) {
+//     res.status(500).json({
+//       error: "Error while looking for the meal",
+//     });
+//   }
+// });
 
 routerMeals.get("/:id", async (req, res) => {
   try {
@@ -83,126 +83,86 @@ routerMeals.delete("/:id", async (req, res) => {
 routerMeals.get("/", async (req, res) => {
   try {
     const query = req.query;
-    // const [keyQuery] = Object.keys(query);
-    // const [valueQuery] = Object.values(query);
-    const { maxPrice } = req.query;
+    const {
+      maxPrice,
+      availableReservations,
+      title,
+      dateAfter,
+      dateBefore,
+      limit,
+      sortKey,
+      sortDir,
+    } = req.query;
+
     console.log(query);
 
     if (query) {
+      // Returns all meals that are cheaper than maxPrice
       if (maxPrice !== undefined) {
-        const resoult = await knex("Meal")
-          .select("Title", "Price")
-          .where("Price", "<", +maxPrice);
-        // if (maxPrice.length) {
-        //   return res.json(resoult);
-        // }
-        res.json(resoult);
+        const result = await knex("Meal")
+          .select("*")
+          .where("Price", "<=", +maxPrice);
+        res.json(result);
       }
-      // switch (keyQuery) {
-      //   case "maxPrice": // Returns all meals that are cheaper than maxPrice
-      //     const maxPrice = await knex("Meal")
-      //       .select("Title", "Price")
-      //       .where("Price", "<", +valueQuery);
-      //     if (maxPrice.length) {
-      //       return res.json(maxPrice);
-      //     }
-      //     res.status(404).json({ error: "Not Found" });
-      //     break;
 
-      //   case "availableReservations": // Returns all meals that still have available spots left, if true. If false, return meals that have no available spots left.
-      //     const availableReservations = await knex
-      //       .select("Title", "status")
-      //       .from("Meal")
-      //       .where("status", "=", "available");
-      //     const unavailableReservations = await knex
-      //       .select("Title", "status")
-      //       .from("Meal")
-      //       .where("status", "=", "unavailable");
-      //     if (valueQuery === "true") {
-      //       if (availableReservations.length) {
-      //         return res.json(availableReservations);
-      //       }
-      //       res.status(404).json({ error: "Not Found" });
-      //     } else if (valueQuery === "false") {
-      //       if (unavailableReservations.length) {
-      //         return res.json(unavailableReservations);
-      //       }
-      //       res.status(404).json({ error: "Not Found" });
-      //     } else {
-      //       res.status(404).json({ error: "Not Found" });
-      //     }
-      //     break;
+      // Returns all meals that still have available spots left, if true. If false, return meals that have no available spots left.
 
-      //   case "title": // Returns all meals that partially match the given title. Rød grød will match the meal with the title Rød grød med fløde
-      //     const title = await knex("Meal")
-      //       .select("Title")
-      //       .where("Title", "like", `%${valueQuery}%`);
-      //     if (title.length) {
-      //       return res.json(title);
-      //     }
-      //     res.status(404).json({ error: "Not Found" });
-      //     break;
+      if (availableReservations !== undefined) {
+        const result = await knex("Meal").where(
+          "Max_reservations",
+          ">",
+          knex.raw(
+            "(SELECT SUM(Number_of_guests) FROM Reservation WHERE Meal_id = Meal.Id)"
+          )
+        );
+        res.json(result);
+      }
 
-      //   case "dateAfter": // Returns all meals where the date for when is after the given date.
-      //     const dateAfter = await knex("Meal")
-      //       .select("Title", "date")
-      //       .where("date", ">", valueQuery);
-      //     if (dateAfter.length) {
-      //       return res.json(dateAfter);
-      //     }
-      //     res.status(404).json({ error: "Not Found" });
-      //     break;
+      // Returns all meals that partially match the given title. Rød grød will match the meal with the title Rød grød med fløde
+      if (title) {
+        console.log("title !== undefined");
+        const result = await knex("Meal")
+          .select("*")
+          .where("Title", "like", `%{title}%`);
+        res.json(result);
+      }
 
-      //   case "dateBefore": // Returns all meals where the date for when is before the given date
-      //     const dateBefore = await knex("Meal")
-      //       .select("Title", "When")
-      //       .where("When", "<", valueQuery);
-      //     if (dateBefore.length) {
-      //       return res.json(dateBefore);
-      //     }
-      //     res.status(404).json({ error: "Not Found" });
-      //     break;
+      // Returns all meals where the date for when is after the given date.
+      if (dateAfter) {
+        const result = await knex("Meal")
+          .select("*")
+          .where("When", ">", dateAfter);
+        res.json(result);
+      }
 
-      //   case "limit": // Returns the given number of meals.
-      //     const limit = await knex("Meal").select("*").limit(+valueQuery);
-      //     if (limit.length) {
-      //       return res.json(limit);
-      //     }
-      //     res.status(404).json({ error: "Not Found" });
-      //     break;
+      // Returns all meals where the date for when is before the given date
+      if (dateBefore) {
+        const result = await knex("Meal")
+          .select("*")
+          .where("When", "<", dateBefore);
+        res.json(result);
+      }
 
-      //   case "sortKey":
-      //     // Returns all meals sorted by the given key. Allows when, max_reservations and price as keys. Default sorting order is asc(ending).
-      //     // Returns all meals sorted in the given direction. Only works combined with the sortKey and allows asc or desc.
-      //     if (
-      //       !query.sortDir &&
-      //       (valueQuery === "Max_reservations" || valueQuery === "Price")
-      //     ) {
-      //       const sortKey = await knex("Meal")
-      //         .select("Title", "Max_reservations", "Price")
-      //         .orderBy(valueQuery);
-      //       return res.json(sortKey);
-      //     } else if (
-      //       (query.sortKey === "Max_reservations" ||
-      //         query.sortKey === "Price") &&
-      //       (query.sortDir === "desc" || query.sortDir === "asc")
-      //     ) {
-      //       const sortDir = await knex("Meal")
-      //         .select("Title", "Max_reservations", "Price")
-      //         .orderBy(query.sortKey, query.sortDir);
-      //       return res.json(sortDir);
-      //     } else {
-      //       res.status(404).json({ error: "Not Found" });
-      //     }
-      //     break;
+      // Returns the given number of meals.
+      if (limit) {
+        const result = await knex("Meal").select("*").limit(limit);
+        res.json(result);
+      }
 
-      //   default:
-      //     const allMeals = await knex("Meal").select("*");
-      //     res.json(allMeals);
-      // }
+      // Apply sorting
+      if (sortKey) {
+        const direction = sortDir === "desc" ? "desc" : "asc";
+        const result = await knex("Meal")
+          .select("*")
+          .orderBy(sortKey, direction);
+        res.json(result);
+      }
     }
+
+    const allMeals = await knex("Meal").select("*");
+    res.json(allMeals);
   } catch (error) {
-    throw error;
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
--- src/backend/api/reservations.js
+++ src/backend/api/reservations.js
@@ -68,4 +68,67 @@ routerReservation.delete("/:Id", async (req, res) => {
   }
 });
 
+routerReservation.post("/meals", async (request, response) => {
+  try {
+    const context = request.body;
+    const requiredParams = [
+      "number_of_guests",
+      "created_date",
+      "contact_phonenumber",
+      "contact_name",
+      "contact_email",
+      "meal_id",
+    ];
+    console.log("context", context);
+    const numberOfGuests = Number(context.number_of_guests);
+    const mealId = Number(context.meal_id);
+    console.log("mealId", mealId);
+
+    console.log("numberOfGuests", numberOfGuests);
+
+    meal = await knex("meals")
+      .select(
+        "meals.id",
+        "meals.title",
+        "meals.description",
+        "meals.location",
+        "meals.when",
+        "meals.created_date",
+        "meals.price",
+        "meals.max_reservations",
+        knex.raw(
+          "(meals.max_reservations-COALESCE(SUM(reservations.number_of_guests),0)) as available_reservation"
+        )
+      )
+      .where("meals.id", "=", mealId)
+      .leftJoin("reservations", "meals.id", "=", "reservations.meal_id");
+    const currentMeal = meal[0];
+    const avRes = currentMeal.available_reservation;
+    console.log("currentMeal", currentMeal);
+    console.log("avRes", avRes);
+
+    if (Object.keys(context).length === requiredParams.length) {
+      if (Number(avRes) < numberOfGuests) {
+        response.status(200).json({
+          message: `Sorry. Not enough available seats on ${currentMeal.title}. It has ${avRes} available seats`,
+        });
+      } else {
+        const insertContext = await knex("reservations").insert(context);
+        response.send(`Thanks for your reservation`).json(insertContext);
+      }
+    } else {
+      requiredParams.map((param) => {
+        if (context[param] === "" || context[param] === undefined) {
+          response.status(422).json({
+            message: "Bad input",
+            hint: `Please, provide ${param}`,
+          });
+        }
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
 module.exports = routerReservation;
