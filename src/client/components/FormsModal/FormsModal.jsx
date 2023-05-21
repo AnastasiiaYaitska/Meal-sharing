bc45e0e6285e4f3d563d83cdea3530d7557ba90b
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-05-21 23:00:50 +0200

    add conditions for disadling form btn

--- src/client/components/FormsModal/FormsModal.jsx
+++ src/client/components/FormsModal/FormsModal.jsx
@@ -4,7 +4,7 @@ import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import { Backdrop } from "./FormsModal.styled";
 
-const FormsModal = ({ children, buttonText }) => {
+const FormsModal = ({ children, buttonText, isDisabled }) => {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -14,11 +14,18 @@ const FormsModal = ({ children, buttonText }) => {
     setOpen(false);
   };
 
+  const configBtn = {
+    onClick: handleOpen,
+    color: "warning",
+    variant: "contained",
+  };
+
+  if (isDisabled === "true") {
+    configBtn.disabled = true;
+  }
   return (
     <>
-      <Button onClick={handleOpen} color="warning" variant="contained">
-        {buttonText}
-      </Button>
+      <Button {...configBtn}>{buttonText}</Button>
       <Modal
         open={open}
         onClose={handleClose}
