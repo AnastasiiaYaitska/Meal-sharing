11b40e1ba402859bf12280f7fd5fcb0a9b7ac58c
AnastasiiaYaitska <yaitska.anastasiia@gmail.com>
2023-05-22 09:23:00 +0200

    fix footer

--- src/client/components/WrapperContainer/WrapperContainer.styled.js
+++ src/client/components/WrapperContainer/WrapperContainer.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const WrapperContainer = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
+  min-height: 90vh;
   flex-direction: column;
   justify-content: flex-start;
 `;
